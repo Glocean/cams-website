88ea1df693ec9c2de3b24a63439f10b29e620d1f
C-Dahr <cameronsdahr@gmail.com>
2024-02-01 23:37:56 -0400

    Added PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import PrimeVue from 'primevue/config';
 
-createApp(App).use(router).mount("#app");
+// Styles
+import 'primevue/resources/themes/aura-dark-teal/theme.css'
+
+//Components
+import Button from "primevue/button"
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(router);
+
+// Use Components
+app.component('p-button', Button);
+
+app.mount("#app");
