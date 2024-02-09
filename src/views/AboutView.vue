edcc069a37f96d68ca075c238de1d6192d00f259
C-Dahr <cameronsdahr@gmail.com>
2024-02-09 18:52:41 -0400

    Added axios call to fetch spreadsheet data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>{{ answer }}</div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AboutView",
+  data() {
+    return {
+      answer: {},
+    }
+  },
+  methods: {
+    async getAnswer() {
+      const request = 'https://sheets.googleapis.com/v4/spreadsheets/1gbykEEXRHrIWTfl6gPrcxXjGZ6BndlAUxWrRcyHIp68/values/A2:H?key='+process.env.VUE_APP_API_KEY
+      console.log(request)
+      const { data } = await axios.get(request);
+      console.log(data)
+      this.answer = data.values
+    }
+  },
+  beforeMount() {
+    this.getAnswer();
+  },
+};
+</script>
