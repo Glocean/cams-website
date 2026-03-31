b2d9ddefe72fb4d467509c3a986436b628006bc3
C-Dahr <cameronsdahr@gmail.com>
2026-03-31 04:07:11 -0300

    Missed one

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -63,7 +63,7 @@
   // TODO: Make images links/add more details (playtime) to timeline
   // TODO: Add little icon/banner that shows 100% completion
   import axios from "axios";
-  import { getBannerUrl, getGamePageUrl, getGames } from "@/scripts/utils";
+  import { getBannerUrl, getGamePageUrl, getIconUrl, getGames } from "@/scripts/utils";
   
   export default {
     name: "Timeline",
@@ -265,18 +265,7 @@
           
         }
       },
-      getIconUrl(data) {
-        var icon;
-        if(data.steamId != null && data.steamId != ""){
-          var id = data.steamId;
-          var hash = data.steamIcon;
-          icon = "http://media.steampowered.com/steamcommunity/public/images/apps/"+id+"/"+hash+".jpg";
-        }else{
-          var title = data.title.toLowerCase().replace(/ /g,"_").replace(/'/g, '');
-          icon = "/game_assets/icons/"+title+"_icon.png";
-        }
-        return icon;
-      },
+      getIconUrl,
       getBannerUrl,
       getGamePageUrl,
       compareDates( a, b ) {
