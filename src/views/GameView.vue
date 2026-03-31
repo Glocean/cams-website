7b9856d45d7f42c7fa116b426bf3d987c6acdb50
C-Dahr <cameronsdahr@gmail.com>
2026-03-31 04:05:08 -0300

    Used helpers in views and fixed data handling bug

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -23,6 +23,7 @@ import { FilterMatchMode, FilterOperator } from '@primevue/core/api'
 import GameTable from "@/components/GameTable.vue";
 import CurrentlyPlaying from "@/components/CurrentlyPlaying.vue";
 import BacklogTable from "@/components/BacklogTable.vue";
+import { getBannerUrl, getGamePageUrl, getIconUrl, getGames } from "@/scripts/utils";
 
 export default {
   name: "GameView",
@@ -54,18 +55,7 @@ export default {
       this.games = null;
       this.currentlyPlaying = [];
       this.backlog = [];
-      const request = 'https://sheets.googleapis.com/v4/spreadsheets/1gbykEEXRHrIWTfl6gPrcxXjGZ6BndlAUxWrRcyHIp68/values/A2:K?key='+import.meta.env.VITE_API_KEY
-      const { data } = await axios.get(request);
-      var input = data.values
-      const keys = ["title", "completion", "date", "hours", "genre", "rating", "reccomend", "return", "steamId", "steamIcon", "notes"];
-      this.games = input.reduce(function(acc, cur, i) {
-        var test = cur.reduce(function(acc, cur, i) {
-          acc[keys[i]] = cur;
-          return acc;
-        }, {});
-        acc[i] = test;
-        return acc;
-      }, []);
+      this.games = await getGames();
       this.loading = false;
       this.games.forEach((item) => {
         if (item.completion == "In Progress") {
@@ -106,23 +96,8 @@ export default {
         
       }
     },
-    getIconUrl(data) {
-      var icon;
-      if(data.steamIcon != ""){
-        var id = data.steamId;
-        var hash = data.steamIcon;
-        icon = "http://media.steampowered.com/steamcommunity/public/images/apps/"+id+"/"+hash+".jpg";
-      }else{
-        var title = data.title.toLowerCase().replace(/ /g,"_").replace(/'/g, '');
-        icon = "/game_assets/icons/"+title+"_icon.png";
-      }
-      return icon;
-    },
-    getBannerUrl(data) {
-      var id = data.steamId;
-      var banner = "https://cdn.akamai.steamstatic.com/steam/apps/"+id+"/header.jpg";
-      return banner;
-    }
+    getIconUrl,
+    getBannerUrl
   },
   beforeMount() {
     this.getGames();
