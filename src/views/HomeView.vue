afa8d01524dc8092bad1ac155b0dccc80e22ee2b
C-Dahr <cameronsdahr@gmail.com>
2026-06-23 17:27:38 -0300

    Fixed some styling and updated picture on the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="flex flex-column align-items-center justify-content-center fadein animation-ease-in animation-duration-1000">  
       <div class="flex flex-row align-items-center justify-content-center flex-wrap m-2">
-        <p-avatar class="h-25rem w-25rem shadow-6" shape="circle" image="/moco.jpg"/>
+        <p-avatar class="h-25rem w-25rem shadow-6" shape="circle" image="/wedding.jpg"/>
         <div class="flex flex-column m-8">
           <div class="flex flex-row align-items-center justify-content-center name">Cameron Dahr</div>
           <div class="flex flex-row align-items-center justify-content-center m-3">
@@ -19,46 +19,46 @@
             <p-button severity="info" text link aria-label="LinkedIn" v-tooltip.bottom="{ value: 'LinkedIn' }">
               <template #icon>
                 <a href="https://www.linkedin.com/in/cameron-dahr-8a943a129/" target="_blank">
-                  <font-awesome-icon class="text-4xl" icon="fa-brands fa-linkedin" color="#00A0DC" />
+                  <font-awesome-icon class="text-4xl" icon="fa-brands fa-linkedin" color="white" />
                 </a>
               </template>
             </p-button>
             <p-button severity="warning" text link aria-label="LetterBoxd" v-tooltip.bottom="{ value: 'Letterboxd' }">
               <template #icon>
                 <a href="https://letterboxd.com/Glocean/" target="_blank">
-                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-square-letterboxd" color="#00e054" />
+                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-square-letterboxd" color="white" />
                 </a>
               </template>
             </p-button>
             <p-button severity="success" text aria-label="Facebook" v-tooltip.bottom="{ value: 'Facebook' }">
               <template #icon>
                 <a href="https://www.facebook.com/cameron.dahr.1" target="_blank">
-                  <font-awesome-icon class="text-4xl" icon="fa-brands fa-square-facebook" color="#1547ad"/>
+                  <font-awesome-icon class="text-4xl" icon="fa-brands fa-square-facebook" color="white"/>
                 </a>
               </template>
             </p-button>
             <p-button severity="help" text aria-label="Twitch" v-tooltip.bottom="{ value: 'Twitch' }">
               <template #icon>
                 <a href="https://www.twitch.tv/glocean" target="_blank">
-                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-twitch" color="#b9a3e3"/>
+                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-twitch" color="white"/>
                 </a>
               </template>
             </p-button>
             <p-button severity="danger" text aria-label="Youtube" v-tooltip.bottom="{ value: 'YouTube' }">
               <template #icon>
                 <a href="https://www.youtube.com/@GloceanTTV" target="_blank">
-                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-youtube" color="#DD0000"/>
+                  <font-awesome-icon class="text-3xl" icon="fa-brands fa-youtube" color="white"/>
                 </a>
               </template>
             </p-button>
           </div>
         </div>
       </div>
-      <div class="flex flex-row align-items-center justify-content-center m-5">
-        <p-card class="shadow-5" style="width: 45%;background:rgba(0, 0, 0, 0.5);">
+      <div class="flex flex-row align-items-left justify-content-center m-5">
+        <p-card class="shadow-5 text-left" style="width: 45%;background:rgba(0, 0, 0, 0.5);">
           <template #content>
-            <p class="card-text">
-              Hi, my name is Cameron, and I am a Full Stack Software Developer. I graduated from the University of New Brunswick in May of 2020 with a degree in Software Engineering. This is the landing page for the website I made using Vue 3 to learn about web deployment. My hobbies include playing video games, collecting board games and dice, reading/writing, and playing Dungeons and Dragons. I also enjoy watching movies, and log movies I watch on Letterboxd (link to my profile above)! I also love the ocean; I hope to one day learn to scuba dive and my favorite animal is an octopus!
+            <p class="card-text pl-3">
+              Hi, my name is Cameron, and I am a Software Development Engineer in Test. I graduated from the University of New Brunswick in May of 2020 with a degree in Software Engineering. This is the landing page for the website I made using Vue 3 to learn about web deployment. My hobbies include playing video games, collecting board games and dice, reading/writing, and playing Tabletop RPGs. I enjoy watching movies, and log movies I watch on Letterboxd (link to my profile above). I also really love the ocean; I hope to one day learn to scuba dive and my favorite animal is an octopus!
             </p>
           </template>
         </p-card>
@@ -91,7 +91,6 @@ export default {
 }
 
 .card-text {
-  justify-content: left;
   font-size: 2vh;
 }
 
